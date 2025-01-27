e4ef7e4052a5de69f9ed42908221866446019584
Hanzla <mhanzla1408@gmail.com>
2025-01-28 00:16:46 +0800

    understand the excecution context

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log(num)
+getName()
+
+
+var num = 4;
+
+function getName() {
+    console.log("javascript")
+}
+
+// var getName = () => {
+//     console.log("js")
+// }
+console.log(getName)
+
